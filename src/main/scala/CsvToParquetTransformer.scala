c63d553a1e1dad779b5059275d91a74736c1d4e4
Victor <victor.nguen@gmail.com>
2021-12-19 07:00:17 +0300

    schema reading from JSON file

--- src/main/scala/CsvToParquetTransformer.scala
+++ src/main/scala/CsvToParquetTransformer.scala
@@ -1,5 +1,9 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+
+import scala.io.Source
 
 object CsvToParquetTransformer extends App{
     val spark: SparkSession = SparkSession.builder()
@@ -8,9 +12,21 @@ object CsvToParquetTransformer extends App{
       .getOrCreate()
 
     spark.sparkContext.setLogLevel("ERROR")
-    val df = spark.read.json("data_files\\Json\\config.json")
+    val df = spark.read
+      .option("multiline","true")
+      .json("F:\\scala_projects\\ExtractTransformLoad\\data_files\\Json\\schema_sample.json")
+      .cache()
     df.printSchema()
     df.show(false)
+
+    //Define custom schema
+    val filePath = Source.fromFile("F:\\scala_projects\\ExtractTransformLoad\\data_files\\Json\\config.json")
+    val bufferedSource = filePath
+    val schemaSource = bufferedSource.getLines.mkString
+    bufferedSource.close()
+    val schemaFromJson = DataType.fromJson(schemaSource).asInstanceOf[StructType]
+    val df2 = spark.read.schema(schemaFromJson).csv("F:\\scala_projects\\ExtractTransformLoad\\data_files\\csv\\Courses.csv")
+    df2.printSchema()
 
 //  def readConfig(pathToConfig: Path): Unit ={
 //    val schema = StructType(Seq(
